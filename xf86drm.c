5eac7c3cf77820a155253fc5291fa800c0a57678
Jonathan Gray <jsg@jsg.id.au>
2015-07-16 10:28:39 -0600

    xf86drm: don't try mknod on OpenBSD
    Don't attempt to create /dev entries on OpenBSD.  They are created in
    advance and the xserver runs with privilege separation.
    
    Signed-off-by: Jonathan Gray <jsg@jsg.id.au>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

--- xf86drm.c
+++ xf86drm.c
@@ -805,7 +805,7 @@ static int drmMatchBusID(const char *id1, const char *id2, int pci_domain_ok)
  * If any other failure happened then it will output error message using
  * drmMsg() call.
  */
-#if !UDEV
+#if !UDEV && !defined(__OpenBSD__)
 static int chown_check_return(const char *path, uid_t owner, gid_t group)
 {
         int rv;
@@ -855,7 +855,7 @@ static int drmOpenDevice(dev_t dev, int minor, int type)
     int             fd;
     mode_t          devmode = DRM_DEV_MODE, serv_mode;
     gid_t           serv_group;
-#if !UDEV
+#if !UDEV && !defined(__OpenBSD__)
     int             isroot  = !geteuid();
     uid_t           user    = DRM_DEV_UID;
     gid_t           group   = DRM_DEV_GID;
@@ -873,6 +873,7 @@ static int drmOpenDevice(dev_t dev, int minor, int type)
         devmode &= ~(S_IXUSR|S_IXGRP|S_IXOTH);
     }
 
+#ifndef __OpenBSD__
 #if !UDEV
     if (stat(DRM_DIR_NAME, &st)) {
         if (!isroot)
@@ -919,6 +920,7 @@ wait_for_udev:
         }
     }
 #endif
+#endif /* __OpenBSD__ */
 
     fd = open(buf, O_RDWR | O_CLOEXEC);
     drmMsg("drmOpenDevice: open result is %d, (%s)\n",
@@ -926,7 +928,7 @@ wait_for_udev:
     if (fd >= 0)
         return fd;
 
-#if !UDEV
+#if !UDEV && !defined(__OpenBSD__)
     /* Check if the device node is not what we expect it to be, and recreate it
      * and try again if so.
      */
