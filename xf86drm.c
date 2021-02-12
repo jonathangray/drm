56df815537f5e42d3227c432a59b4251273d27bd
Jonathan Gray <jsg@jsg.id.au>
2021-02-12 12:44:05 +1100

    xf86drm: support OpenBSD xserver privilege separation

--- xf86drm.c
+++ xf86drm.c
@@ -101,6 +101,10 @@
 #define DRM_MAJOR 226 /* Linux */
 #endif
 
+#ifdef __OpenBSD__
+#define X_PRIVSEP
+#endif
+
 #if defined(__OpenBSD__) || defined(__DragonFly__)
 struct drm_pciinfo {
 	uint16_t	domain;
@@ -762,6 +766,18 @@ static const char *drmGetDeviceName(int type)
     return NULL;
 }
 
+#ifdef X_PRIVSEP
+static int
+_priv_open_device(const char *path)
+{
+	drmMsg("_priv_open_device\n");
+	return open(path, O_RDWR, 0);
+}
+
+drm_public int priv_open_device(const char *)
+	__attribute__((weak, alias ("_priv_open_device")));
+#endif
+
 /**
  * Open the DRM device, creating it if necessary.
  *
@@ -850,7 +866,11 @@ wait_for_udev:
 #endif
 #endif /* __OpenBSD__ */
 
+#ifndef X_PRIVSEP
     fd = open(buf, O_RDWR | O_CLOEXEC, 0);
+#else
+    fd = priv_open_device(buf);
+#endif
     drmMsg("drmOpenDevice: open result is %d, (%s)\n",
            fd, fd < 0 ? strerror(errno) : "OK");
     if (fd >= 0)
@@ -908,8 +928,13 @@ static int drmOpenMinor(int minor, int create, int type)
         return -EINVAL;
 
     sprintf(buf, dev_name, DRM_DIR_NAME, minor);
-    if ((fd = open(buf, O_RDWR | O_CLOEXEC, 0)) >= 0)
-        return fd;
+#ifndef X_PRIVSEP
+    fd = open(buf, O_RDWR | O_CLOEXEC, 0);
+#else
+    fd = priv_open_device(buf);
+#endif
+    if (fd >= 0)
+	return fd;
     return -errno;
 }
 
