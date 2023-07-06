df71cdeb5a3f01bd6334fc6adc21a1a28cdb26ff
Matthieu Herrb <matthieu@openbsd.org>
2023-07-06 01:21:30 -0600

    fix drmGetMinorNameForFD(). tweaks and ok jsg@.
    For the gpu n, the main device node is /dev/dri/card<n> and the
    render device node is /dev/dri/renderD<n+drmGetMinorBase()> not
    /dev/dri/renderD<n>
    and miod@ checked that no port should be affected.

--- xf86drm.c
+++ xf86drm.c
@@ -3531,6 +3531,10 @@ static char *drmGetMinorNameForFD(int fd, int type)
     const char *dev_name = drmGetDeviceName(type);
     unsigned int maj, min;
     int n;
+    int base = drmGetMinorBase(type);
+
+    if (base < 0)
+        return NULL;
 
     if (fstat(fd, &sbuf))
         return NULL;
@@ -3544,7 +3548,7 @@ static char *drmGetMinorNameForFD(int fd, int type)
     if (!dev_name)
         return NULL;
 
-    n = snprintf(buf, sizeof(buf), dev_name, DRM_DIR_NAME, min);
+    n = snprintf(buf, sizeof(buf), dev_name, DRM_DIR_NAME, base + min);
     if (n == -1 || n >= sizeof(buf))
         return NULL;
 
